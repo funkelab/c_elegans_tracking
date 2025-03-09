5c67adecbd79e52f7c2c137b113f43d0d069dde2
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-03-08 20:45:24 -0500

    Uncomment actual solving in experiment script

--- experiments/run_tracking.py
+++ experiments/run_tracking.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     solver_params = experiment.solver_params
     seg_centers = ds.seg_centers
     print(len(seg_centers))
-    
+
     cand_graph, conflict_sets = create_cand_graph(
         ds.seg_centers,
         ds.lattice_points,
@@ -44,13 +44,13 @@ if __name__ == "__main__":
     print(
         f"cand graph has {len(cand_graph.nodes)} nodes and {len(cand_graph.edges)} edges"
     )
-    # print("conflict sets", conflict_sets)
-    # experiment.candidate_graph = cand_graph
-    
-    # solver, soln_graph = run_motile(cand_graph, conflict_sets, solver_params)
-    # print(
-    #     f"soln graph has {soln_graph.number_of_nodes} nodes and {soln_graph.number_of_edges} edges"
-    # )
+    print("conflict sets", conflict_sets)
+    experiment.candidate_graph = cand_graph
 
-    # experiment.solution_graph = soln_graph
-    # print(experiment.exp_base_dir)
+    solver, soln_graph = run_motile(cand_graph, conflict_sets, solver_params)
+    print(
+        f"soln graph has {soln_graph.number_of_nodes} nodes and {soln_graph.number_of_edges} edges"
+    )
+
+    experiment.solution_graph = soln_graph
+    print(experiment.exp_base_dir)
